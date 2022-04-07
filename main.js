f1c596b23821bf012f087adc7ef8afd1f864405f
SuperbSamanvi <90319308+SuperbSamanvi@users.noreply.github.com>
2022-04-07 15:58:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+
+    
+    var updatedImage = ["c", "https://st2.depositphotos.com/27811286/46567/v/600/depositphotos_465676542-stock-illustration-young-good-looking-woman-doing.jpg", "https://img.freepik.com/free-vector/little-boy-pointing-with-index-finger-flat-vector-illustration_71593-566.jpg?w=2000", "https://i.pinimg.com/736x/a7/a1/76/a7a176f0cdc6e73ba9ad5b8df2c09895.jpg" ];
+    console.log(updatedImage);
+    document.getElementById("family_member_image").src = updatedImage;vv
+
+    var first_image= updatedImage[0];
+    console.log(first_image);
+    document.getElementById("access_first_element").innerHTML= first_image;
+
+    var second_image= updatedImage[1];
+    console.log(second_image);
+    document.getElementById("access_second_element").innerHTML= second_image;
+
+    var third_image= updatedImage[2];
+    console.log(third_image);
+    document.getElementById("access_third_element").innerHTML= third_image;
+
+    var updatedName = ["Manoj", "Neetu", "Yuvan", "Samanvi"] ;
+    console.log(updatedName);
+    document.getElementById("family_member_name").innerHTML = updatedName;
+
+    var first_name= updatedName[0];
+    console.log(first_name);
+    document.getElementById("access_first_element").innerHTML = first_name;
+      
+    var second_name= update[1];
+    console.log(second_name);
+    document.getElementById("access_second_element").innerHTML = second_name;
+        
+    var third_name= updatedName;
+    console.log(third_name);
+    document.getElementById("access_thrd_element").innerHTML= third_name;
+   
+ 
+    
+    
+
